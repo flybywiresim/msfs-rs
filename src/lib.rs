e0506f046442d907eb0d2f110dec06452742202f
Gus Caplan <me@gus.host>
2020-10-09 18:38:33 -0500

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod sys;
 
 // Prevent compilation of non wasm32-wasi targets
 #[cfg(not(target_os = "wasi"))]
+#[doc(hidden)]
 fn invalid() {
     let _: [(); 0] = [0]; // This library only supports wasm32-wasi
 }
--- src/msfs.rs
+++ src/msfs.rs
@@ -1,8 +1,19 @@
 use crate::sys;
 
+/// MSFS Context. Hashable.
+#[derive(PartialEq, Hash)]
 pub struct FsContext(sys::FsContext);
 
+impl From<sys::FsContext> for FsContext {
+    fn from(ctx: sys::FsContext) -> FsContext {
+        FsContext(ctx)
+    }
+}
+
+/// `PanelServiceID` is used in `GaugeCallback`s and is generated from
+/// `sys::PANEL_SERVICE_*` constants.
 #[repr(u32)]
+#[derive(PartialEq, Clone, Copy)]
 pub enum PanelServiceID {
     PreQuery = sys::PANEL_SERVICE_PRE_QUERY,
     PostQuery = sys::PANEL_SERVICE_POST_QUERY,
@@ -24,15 +35,18 @@ pub enum PanelServiceID {
     PanelClose = sys::PANEL_SERVICE_PANEL_CLOSE,
 }
 
-impl From<sys::FsContext> for FsContext {
-    fn from(ctx: sys::FsContext) -> FsContext {
-        FsContext(ctx)
-    }
-}
-
-pub type GuageCallbackResult = Result<(), ()>;
+/// MSFS Gauges are managed using this lifecycle callback.
 pub type GaugeCallback = fn(&FsContext, PanelServiceID) -> GuageCallbackResult;
+pub type GuageCallbackResult = Result<(), ()>;
 
+/// `gauge!` is used to generate the exported function which is exposed to
+/// MSFS when it loads the WASM binary. Use it like so:
+/// ```rs
+/// fn my_gauge(ctx: &FsContext, servce_id: PanelServiceID) -> GaugeResult {
+///     Ok(())
+/// }
+/// gauge!(my_gauge);
+/// ```
 // FIXME: this should be a proc macro like:
 // ```rs
 // #[msfs::gauge]
