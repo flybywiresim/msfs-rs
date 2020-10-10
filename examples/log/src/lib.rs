57e49b1682f1b3e3f392a6288a5c711687dd7f8d
Gus Caplan <me@gus.host>
2020-10-10 12:24:04 -0500

    add example cfg

--- examples/log/src/lib.rs
+++ examples/log/src/lib.rs
@@ -2,8 +2,15 @@
 
 use msfs::msfs;
 
-#[msfs::gauge(name=FBW)]
-fn fbw(_: &msfs::FsContext, service_id: msfs::PanelServiceID) -> msfs::GaugeCallbackResult {
+/// ```cfg
+/// [VCockpit0]
+/// size_mm=0,0
+/// pixel_size=0,0
+/// texture=$PDF
+/// htmlgauge00=WasmInstrument/WasmInstrument.html?wasm_module=log.wasm&wasm_gauge=LOG, 0,0,0,0
+/// ```
+#[msfs::gauge(name=LOG)]
+fn log(_: &msfs::FsContext, service_id: msfs::PanelServiceID) -> msfs::GaugeCallbackResult {
     println!("RUST: FBWCB {:?}", service_id);
     Ok(())
 }
