73e3fdc549485a969560ce004dd8fbd802351805
Gus Caplan <me@gus.host>
2020-10-09 23:48:14 -0500

    support renaming exported gauge callback

--- msfs_derive/src/lib.rs
+++ msfs_derive/src/lib.rs
@@ -1,7 +1,28 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::ItemFn;
+use syn::{
+    parse::{Parse, ParseStream, Result as SynResult},
+    parse_macro_input, Ident, ItemFn, Token,
+};
+
+struct Args {
+    name: Option<String>,
+}
+
+impl Parse for Args {
+    fn parse(input: ParseStream) -> SynResult<Self> {
+        match input.parse::<Ident>() {
+            Ok(i) if i == "name" => {
+                input.parse::<Token![=]>()?;
+                Ok(Args {
+                    name: Some(input.parse::<Ident>()?.to_string()),
+                })
+            }
+            _ => Ok(Args { name: None })
+        }
+    }
+}
 
 /// Declare a gauge callback. It will be automatically exported with the name
 /// `NAME_gauge_callback`, where `NAME` is the name of the decorated function.
@@ -12,12 +33,22 @@ use syn::ItemFn;
 ///   // ...
 /// }
 /// ```
+///
+/// The macro can also be given a parameter, `name`, to rename the exported function.
+/// ```rs
+/// // Declare and export `FOO_gauge_callback`
+/// #[msfs::gauge(name=FOO)]
+/// fn xyz(...) {}
 #[proc_macro_attribute]
-pub fn gauge(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(item as ItemFn);
+pub fn gauge(args: TokenStream, item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(args as Args);
+    let input = parse_macro_input!(item as ItemFn);
 
     let rusty_name = format_ident!("{}", input.sig.ident);
-    let extern_name = format_ident!("{}_gauge_callback", input.sig.ident);
+    let extern_name = format_ident!(
+        "{}_gauge_callback",
+        args.name.unwrap_or_else(|| input.sig.ident.to_string())
+    );
 
     let output = quote! {
         #input
--- tests/test.rs
+++ tests/test.rs
@@ -2,8 +2,8 @@ extern crate msfs as msfs_root;
 
 use msfs_root::{msfs, sim_connect::SimConnect};
 
-#[msfs::gauge]
-pub fn x(_: &msfs::FsContext, service_id: msfs::PanelServiceID) -> msfs::GaugeCallbackResult {
+#[msfs::gauge(name=FBW)]
+pub fn fbw(_: &msfs::FsContext, service_id: msfs::PanelServiceID) -> msfs::GaugeCallbackResult {
     match service_id {
         msfs::PanelServiceID::PreInstall => {
             let _ = SimConnect::open("test").unwrap();
