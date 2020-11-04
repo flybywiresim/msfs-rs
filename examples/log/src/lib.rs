8e4d279556a3b78f875e830fb880f35e352ac8f6
Gus Caplan <me@gus.host>
2020-11-04 10:24:34 -0600

    update example

--- examples/log/src/lib.rs
+++ examples/log/src/lib.rs
@@ -1,6 +1,11 @@
-use ::msfs::{msfs, msfs::MSFSEvent, sim_connect::data_definition};
+use ::msfs::{
+    msfs,
+    msfs::MSFSEvent,
+    sim_connect::{data_definition, Period, SimConnectRecv, SIMCONNECT_OBJECT_ID_USER},
+};
 
 #[data_definition]
+#[derive(Debug)]
 struct ControlSurfaces {
     #[name = "ELEVATOR POSITION"]
     #[unit = "Position"]
@@ -22,7 +27,7 @@ struct ControlSurfaces {
 /// ```
 #[msfs::gauge(name=LOG)]
 async fn log(mut gauge: msfs::Gauge) -> Result<(), Box<dyn std::error::Error>> {
-    let sim = gauge.open_simconnect("LOG")?;
+    let mut sim = gauge.open_simconnect("LOG")?;
 
     while let Some(event) = gauge.next_event().await {
         println!("RUST: EVENT {:?}", event);
@@ -30,11 +35,22 @@ async fn log(mut gauge: msfs::Gauge) -> Result<(), Box<dyn std::error::Error>> {
         match event {
             MSFSEvent::PanelServiceID(service_id) => match service_id {
                 msfs::PanelServiceID::PostInstall => {
-                    sim.add_data_definition::<ControlSurfaces>(0)?;
+                    sim.add_data_definition::<ControlSurfaces>()?;
+                    sim.request_data_on_sim_object::<ControlSurfaces>(
+                        0,
+                        SIMCONNECT_OBJECT_ID_USER,
+                        Period::SimFrame,
+                    )?;
                 }
                 _ => {}
             },
-            MSFSEvent::SimConnect(_recv) => {}
+            MSFSEvent::SimConnect(recv) => match recv {
+                SimConnectRecv::SimObjectData(event) => {
+                    let data = event.into::<ControlSurfaces>(&sim).unwrap();
+                    println!("{:#?}", data);
+                }
+                _ => {}
+            },
         }
     }
 
