395d73c8d2414434eaedd89ee1e9cba6f7a8b7d9
Gus Caplan <me@gus.host>
2020-12-15 14:08:22 -0600

    add a note about compilation

--- msfs/src/lib.rs
+++ msfs/src/lib.rs
@@ -7,13 +7,16 @@
 //!
 //! ## Building
 //!
+//! Tools such as `cargo-wasi` may not work. When in doubt, try invoking
+//! `cargo build --target wasm32-wasi` directly.
+//!
 //! If your MSFS SDK is not installed to `C:\MSFS SDK` you will need to set the
 //! `MSFS_SDK` env variable to the correct path.
 //!
 //! ## Known Issues and Work-Arounds
 //!
 //! ### Missing various exports
-//! Add a .cargo/config.toml file with the following settings:
+//! Add a local `.cargo/config.toml` file with the following settings:
 //! ```toml
 //! [target.wasm32-wasi]
 //! rustflags = [
