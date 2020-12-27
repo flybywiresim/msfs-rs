a4edff3affc843670cffb9953a3b615220eaa449
devsnek <me@gus.host>
2020-12-27 05:11:24 +0000

    Deploying to gh-pages from  @ 7a0ccdd7fbcbe770685e8366715b180f3f16ecaf 🚀

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["msfs"] = [{"text":"impl Drop for SimConnect","synthetic":false,"types":[]},{"text":"impl Drop for Context","synthetic":false,"types":[]},{"text":"impl Drop for Image","synthetic":false,"types":[]}];
+implementors["msfs"] = [{"text":"impl&lt;'a&gt; Drop for SimConnect&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl Drop for Context","synthetic":false,"types":[]},{"text":"impl Drop for Image","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
