02a3b7f98acc7d4fa4a7dfd40e9e62f0a5127912
Saschl <sascharudolf46@gmail.com>
2025-02-17 02:31:49 +0100

    try

--- msfs/src/legacy.rs
+++ msfs/src/legacy.rs
@@ -169,7 +169,7 @@ impl AircraftVariableApi {
        // let leakedBox = Box::leak(paramsArrayP);
         let params = sys::FsVarParamArray {
             size: 1 as u32,
-            array: Box::into_raw(paramsArrayP),
+            array: Box::<[sys::FsVarParamVariant]>::into_raw(paramsArrayP),
         };
 
        // std::mem::forget(paramsArrayP);
