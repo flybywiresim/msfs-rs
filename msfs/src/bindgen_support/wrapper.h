c7a3c7637efcfa55469976e5dd4edd7f706282e3
Lukas Michels <lukasmichels00@gmail.com>
2026-06-03 11:25:43 +0200

    feat: include new headers if 2024 feature is active

--- msfs/src/bindgen_support/wrapper.h
+++ msfs/src/bindgen_support/wrapper.h
@@ -7,3 +7,10 @@
 #include <MSFS/MSFS_CommBus.h>
 #include <MSFS/Render/nanovg.h>
 #include <SimConnect.h>
+
+#ifdef INCLUDE_MSFS_2024
+#include <MSFS/MSFS_Vars.h>
+#include <MSFS/MSFS_SystemContext.h>
+#include <MSFS/MSFS_Events.h>
+#include <MSFS/Types/MSFS_EventsEnum.h>
+#endif
